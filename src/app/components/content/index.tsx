d2b89ab35fec0598d09fba454a28e35c32e88646
Deris Hardiansyah Ismail <derishardiansyah27@gmail.com>
2023-08-29 10:03:22 +0700

    update product

--- src/app/components/content/index.tsx
+++ src/app/components/content/index.tsx
@@ -1,7 +1,63 @@
-import React from "react";
+import Image from "next/image";
+import classes from "./style.module.scss";
 
 const Content = () => {
-  return <div>index</div>;
+  return (
+    <div className={classes.content}>
+      <div className={classes.imageProduct}>
+        <div className={classes.thumbnail}>
+          <Image
+            src="/Assets/images/image-product-1.jpg"
+            width={400}
+            height={400}
+            alt="ThumbnailProduct"
+          />
+        </div>
+        <div className={classes.product}>
+          <Image
+            src="/Assets/images/image-product-1.jpg"
+            width={62}
+            height={62}
+            alt="Product"
+          />
+          <Image
+            src="/Assets/images/image-product-2.jpg"
+            width={62}
+            height={62}
+            alt="Product"
+          />
+          <Image
+            src="/Assets/images/image-product-3.jpg"
+            width={62}
+            height={62}
+            alt="Product"
+          />
+          <Image
+            src="/Assets/images/image-product-4.jpg"
+            width={62}
+            height={62}
+            alt="Product"
+          />
+        </div>
+      </div>
+      <div className={classes.description}>
+        <div className={classes.productCompany}>sneaker company</div>
+        <div className={classes.productName}>Fall Limited Edition Sneakers</div>
+        <div className={classes.productDescription}>
+          These low-profile sneakers are your perfect casual wear companion.
+          Featuring a durable rubber outer sole, they’ll withstand everything
+          the weather can offer.
+        </div>
+        <div className={classes.price}>
+          <div className={classes.priceDiscount}>$125.00</div>
+          <div className={classes.box}>
+            <div className={classes.discount}>50%</div>
+          </div>
+        </div>
+        <div className={classes.normalPrice}></div>
+      </div>
+    </div>
+  );
 };
 
 export default Content;
--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,7 +1,52 @@
-import React from "react";
+import Image from "next/image";
+import classes from "./style.module.scss";
 
 const Navbar = () => {
-  return <div> Navbar</div>;
+  return (
+    <div className={classes.navbar}>
+      <div className={classes.navbarBrand}>
+        <div className={classes.leftBrand}>
+          <div className={classes.titleBrand}>sneakers</div>
+          <div className={classes.menuBrand}>
+            <div>Collections</div>
+            <div>Men</div>
+            <div>Women</div>
+            <div>About</div>
+            <div>Contact</div>
+          </div>
+        </div>
+        <div className={classes.rightNavbar}>
+          <div className={classes.cart}>
+            <Image
+              src="/Assets/images/icon-cart.svg"
+              width={20}
+              height={20}
+              alt="Cart"
+            />
+          </div>
+          <div className={classes.profileNavbar}>
+            <Image
+              src="/Assets/images/image-avatar.png"
+              width={40}
+              height={40}
+              alt="Picture of the author"
+            />
+          </div>
+        </div>
+      </div>
+      <div className={classes.vector}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1141"
+          height="3"
+          viewBox="0 0 1141 3"
+          fill="none"
+        >
+          <path d="M0 2L1141 1" stroke="#F7F8FD" stroke-width="2" />
+        </svg>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
